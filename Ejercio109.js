fa0c8bf91089a560c84150201bf629485bb75558
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:51:33 -0500

    Create Ejercio109.js

---
+++ Ejercio109.js
@@ -0,0 +1,11 @@
+// lib/math.js
+export ​let sum = (x, y) => { return x + y; }
+
+export ​let pi = 3.14
+
+
+// app.j
+
+import * ​as math from "lib/mat
+"
+console.log(`2p = + ${math.sum(math.pi, math.pi)
