bb767fd8d0b5de558a60e7a738c3c84510381832
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:46:17 -0500

    Create Ejercio91.js

---
+++ Ejercio91.js
@@ -0,0 +1,6 @@
+<div>
+  <button onclick="prev()"> Prev </button>
+  <img id="slider" src="http://www.sololearn.com/uploads/slider/1.jpg" 
+    width="200px" height="100px"/>
+  <button onclick="next()"> Next </button>
+</div>
