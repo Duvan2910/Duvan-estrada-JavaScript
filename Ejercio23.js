985fca9845e18bf85493ad628fa67896aeaec803
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:17:28 -0500

    Create Ejercio23.js

---
+++ Ejercio23.js
@@ -0,0 +1,12 @@
+var course = 3;
+if (course==1) {
+
+    document.write("<h1>HTML Tutirial</h1>")
+}else if (course == 2) {
+
+    document.write("<h1>css Tutorial</h1>"){
+
+
+    }else {
+        document.write("<h1>Javascript Tutorial</h1>")    
+}
