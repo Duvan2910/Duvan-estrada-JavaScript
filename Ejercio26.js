3adb5b73e88c67d0019edada34d6d691827be76a
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:18:23 -0500

    Create Ejercio26.js

---
+++ Ejercio26.js
@@ -0,0 +1,18 @@
+var color ="yellow";
+switch(color) {
+
+    case "blue":
+        document.write("This is blue.");
+        break;
+    case "red":
+        document.write("this is red.");
+        break;
+        case "green":
+            document.write("this is green.");
+            break;
+    case "orange":
+        document.write("this is orange.")
+        break;
+    default:
+        document.write("color not found.");
+}
