230acfc181a2802325715ceee5f7b9abb6502e3d
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:28:15 -0500

    Create Ejercio48.js

---
+++ Ejercio48.js
@@ -0,0 +1,11 @@
+var result = confirm("Do you really want to leave this page?")
+if (result == true) {
+
+    alert("thanks for visting");
+
+}
+
+else {
+
+    alert("thanks for staying with us");
+}
