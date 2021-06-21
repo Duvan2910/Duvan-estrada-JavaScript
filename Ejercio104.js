8108120a7a961acabf36879a75dd336f90c9345b
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:50:04 -0500

    Create Ejercio104.js

---
+++ Ejercio104.js
@@ -0,0 +1,19 @@
+var square = class Rectangle {
+
+    constructor(height,width)
+
+    this.height = height
+    this.width = width
+  }
+
+  get area() {
+      return this.calcArea();
+  }
+
+  calcArea() {
+
+    return this.height * this.width;
+  }
+
+  const square = new Rectangle(5 , 5);
+  console.log(square.area); // 25
