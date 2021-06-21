84d8ab755fd4ef833d8babb757cd68e87fcf6b05
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:43:01 -0500

    Create Ejercio83.js

---
+++ Ejercio83.js
@@ -0,0 +1,22 @@
+<style>
+  
+  #container {
+
+      with: 200px;
+      height : 200px;
+      background: green;
+      position: relative;
+
+  }
+  #box {
+
+      with: 50px;
+      height: 50px;
+      background:reb;
+      position: absolute;
+  }
+</style>
+<div id ="container">
+
+    <div id="box"></div>
+</div>
