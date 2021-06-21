e6ac2d03a09a6802d3dbed16c7e28a749b302ab5
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:48:02 -0500

    Create Ejercio97.js

---
+++ Ejercio97.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+
+    var sum = x+y;
+    console.log(sum)
+}
+
+const add = (x, y) => {
+
+    let sum = x + y;
+     console.log(sum);
+}
+
+const greet = x => "welcome" + x;
+
+const x () => alert("hi");
+
+var arr = [2,3,7,8];
+arr.forEach(function(el) {
+
+  console.log(el + 2);
+});
