df62bfdc465ac1f2a6269c216926e9df1cef812a
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:46:34 -0500

    Create Ejercio92.js

---
+++ Ejercio92.js
@@ -0,0 +1,22 @@
+<form onsubmit="return validate()" method="post">
+  Number: <input type="text" name="num1" id="num1" />
+  <br />
+  Repeat: <input type="text" name="num2" id="num2" />
+  <br />
+  <input type="submit" value="Submit" />
+</form>
+
+function validate () {
+
+    var n1 = document.getElementById("num1");
+    var n2 = document.getElementById("num2");
+    if(n1.value != "" && n2.value != ""){
+        if(n1.value == n2.value){
+            return true;
+        }
+    }
+    
+}
+
+alert ("the values should be aqual and not blank");
+return false;
