508b1b492363fcca49d54e02aad708cf7ce5bd0b
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:48:36 -0500

    Create Ejercio99.js

---
+++ Ejercio99.js
@@ -0,0 +1,18 @@
+let prop = 'name';
+let id = '1234';
+lest mobile = '08923'
+
+let user ={
+
+    [prop] : 'jack',
+    ['user_${id}']: ´${mobile}´
+
+};
+
+vat i = 0;
+var a = {
+
+    ['foo' + ++i]: i,
+    ['foo' + ++i]: i,
+    ['foo' + ++i]: i,
+};
