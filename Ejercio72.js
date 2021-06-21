f4b565c21438c6eea139e854664d90a2e1cfb2ff
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:39:55 -0500

    Create Ejercio72.js

---
+++ Ejercio72.js
@@ -0,0 +1,15 @@
+var d = new Date();
+//d stores the current date and time
+
+new Date(milliseconds)
+new Date(dateString)
+new Date(year, month, day, hours, minutes, seconds, milliseconds)
+
+//Fri Jan 02 1970 00:00:00
+var d1 = new Date(86400000); 
+
+//Fri Jan 02 2015 10:42:00
+var d2 = new Date("January 2, 2015 10:42:00");
+
+//Sat Jun 11 1988 11:42:00
+var d3 = new Date(88,5,11,11,42,0,0);
