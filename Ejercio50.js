c83e46532254b920a1ba07889dfaa9914fb4507d
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:29:38 -0500

    Create Ejercio50.js

---
+++ Ejercio50.js
@@ -0,0 +1,14 @@
+objectName.propertyName
+//or
+objectName['propertyName']
+
+Var person = {
+
+    name: "john", height: 183
+};
+
+var x = person.age;
+var y = person['age'];
+
+var course = {name: "js",lessons:41}
+document.write(course.name.length);
