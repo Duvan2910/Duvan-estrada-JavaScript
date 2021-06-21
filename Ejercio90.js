998466b02b989ff1193459ffb5bb250a2adbeaa5
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:46:02 -0500

    Create Ejercio90.js

---
+++ Ejercio90.js
@@ -0,0 +1,15 @@
+<div>
+
+    <button>prev</button>
+    <img id="slider"
+src="http//www.sololearn.com/uploads/slider/1.jpg"
+width="200px" height="100px"/>
+<button> Next </button>
+</div>
+
+
+var images = [
+    "http://www.sololearn.com/uploads/slider/1.jpg", 
+    "http://www.sololearn.com/uploads/slider/2.jpg", 
+    "http://www.sololearn.com/uploads/slider/3.jpg"
+ ];
