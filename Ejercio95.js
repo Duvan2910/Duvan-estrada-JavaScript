b774d2d1ac0e8d2089058ccbcabf2aaf906ec99c
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:47:28 -0500

    Create Ejercio95.js

---
+++ Ejercio95.js
@@ -0,0 +1,12 @@
+lat name = 'david';
+let msg = 'welcome' +name +'!';
+console. log (msg);
+
+lat name = 'david';
+let msg = 'welcome' $name +'!';
+console. log (msg);
+
+let a = 8;
+let b = 34;
+let msg = 'the sum is ${a+b}';
+console . log(msg);
