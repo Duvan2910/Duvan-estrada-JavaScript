918163cbfac7322fa5045550328426c0a33a7911
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:42:32 -0500

    Create Ejercio81.js

---
+++ Ejercio81.js
@@ -0,0 +1,11 @@
+<div id ="demo">
+    <p id="p1">this is a paragraph.</p>
+    <p id="p2">this is another paragraph.</p>
+</div>
+
+<script>
+
+    var parent = document.getElementById("demo");
+    var child = document.getElementById("p1");
+    parent.removechild(child);
+</script>
