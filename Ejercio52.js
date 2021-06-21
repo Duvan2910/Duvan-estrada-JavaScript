cc8cd240cdf57d5b8e784e608516af27baa6120a
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:30:12 -0500

    Create Ejercio52.js

---
+++ Ejercio52.js
@@ -0,0 +1,9 @@
+var person = {
+    name: "John", age: 42, favColor: "green"
+    };
+
+    function person(name, age, color) {
+        this.name = name;
+        this.age = age;
+        this.favColor = color;
+      }
