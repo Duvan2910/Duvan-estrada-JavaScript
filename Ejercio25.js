eafbacdf2ef196e4585ff86a7a57da564d79c517
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:18:04 -0500

    Create Ejercio25.js

---
+++ Ejercio25.js
@@ -0,0 +1,14 @@
+Var day =2;
+switch (day){
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    default:
+        document.write("another day");
+}
