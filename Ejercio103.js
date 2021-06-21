f052361a320fb09828d823d0dbb2207ce68b09f3
Duvan2910 <82674179+Duvan2910@users.noreply.github.com>
2021-06-21 11:49:46 -0500

    Create Ejercio103.js

---
+++ Ejercio103.js
@@ -0,0 +1,17 @@
+class Rectangle {
+    constructor(height, width) {
+      this.height = height;
+      this.width = width;
+    }
+  }
+
+  const square = new Rectangle(5,  5);
+  const square = new Rectangle(5,  3);
+
+  var square = class Rectangle {
+
+    constructor(height,width)
+
+    this.height = height
+    this.width = width
+  }
